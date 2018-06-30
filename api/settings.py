80f06c16accec7fff43ac85d86e08162318ace7c
Michael Berber <mickberber@icloud.com>
2018-06-30 14:11:03 -0700

    add changes

--- api/settings.py
+++ api/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0q*#m$a4+h+-=cah-$(&k$t*6_=0+olotk471lv(6wf$s!9vsa'
+SECRET_KEY = 'CHANGE ME'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
